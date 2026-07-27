f5e9748ba0b52bf66f8ec3183ad78fb90b385d1b
kabirvvv <kabirv338@gmail.com>
2026-07-27 23:52:08 +0530

    Update SignInPage.jsx

--- src/Pages/SignInPage.jsx
+++ src/Pages/SignInPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from "../context/AuthContext";
 
 export default function SignInPage() {
   const { signInWithGoogle, signUpWithEmail, signInWithEmail } = useAuth();
