6f4ed94d129a05431f9a46eaeca170bf6aca910e
kabirvvv <kabirv338@gmail.com>
2026-07-28 13:55:42 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 import { PlayerProvider } from './context/PlayerContext.jsx'
 import { LibraryProvider } from './context/LibraryContext.jsx'
 import { ProfileProvider } from './context/ProfileContext.jsx'
@@ -10,13 +11,15 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <LibraryProvider>
-        <ProfileProvider>
-          <PlayerProvider>
-            <App />
-          </PlayerProvider>
-        </ProfileProvider>
-      </LibraryProvider>
+      <AuthProvider>
+        <LibraryProvider>
+          <ProfileProvider>
+            <PlayerProvider>
+              <App />
+            </PlayerProvider>
+          </ProfileProvider>
+        </LibraryProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
